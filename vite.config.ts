2beb0963a87d5aa0ce73c00f9c8966a3bfea6458
zeromountain <sonyeongsan@gmail.com>
2025-05-25 21:01:30 +0900

    👀 project init

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+      "@components": resolve(__dirname, "./src/components"),
+      "@assets": resolve(__dirname, "./src/assets"),
+      "@styles": resolve(__dirname, "./src/styles"),
+      "@utils": resolve(__dirname, "./src/utils"),
+      "@hooks": resolve(__dirname, "./src/hooks"),
+      "@pages": resolve(__dirname, "./src/pages"),
+    },
+  },
+});
