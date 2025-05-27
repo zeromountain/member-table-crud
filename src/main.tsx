483face9360034305d24942af994012db52566eb
zeromountain <sonyeongsan@gmail.com>
2025-05-27 12:03:05 +0900

    ➖ remove field manager

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,14 @@ import 'antd/dist/reset.css';
 
 import '@ant-design/v5-patch-for-react-19';
 
-import { FieldManagerProvider } from '@contexts/FieldManager.tsx';
 import { MemberModalProvider } from '@contexts/MemberModalContext.tsx';
 
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FieldManagerProvider>
-      <MemberModalProvider>
-        <App />
-      </MemberModalProvider>
-    </FieldManagerProvider>
+    <MemberModalProvider>
+      <App />
+    </MemberModalProvider>
   </StrictMode>,
 );
